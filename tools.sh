7d615059b7efa237ca817b689c93957c612ea5df
XIE Qianyue <xie.qianyue@gmail.com>
2020-02-26 16:33:34 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -9,6 +9,12 @@ echo "${verion/.}"
 # cut string
 echo "Docker Root Dir: /data/docker" | cut -f 2 -d ":"
 
+# get column by awk
+# df -h /var/lib/docker
+# 文件系统        容量  已用  可用 已用% 挂载点
+# /dev/sdb3       931G  123G  808G   14% /data
+df -h /var/lib/docker | tail -1 | awk '{print $5}'
+
 # check if argument exists
 if [ -z "$1" ]; then
     echo "No argument supplied"
